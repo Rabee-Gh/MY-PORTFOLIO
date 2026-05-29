4a414fb853b0ef20a4789349c8ee3dac9abd0a3a
Rabee-Gh <rabeeghanem0@gmail.com>
2026-05-29 16:58:13 +0300

    link updates

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -16,14 +16,14 @@ const Footer = ({ data, isEnglish }) => {
   return (
     <footer className="simple-footer">
       <div className="footer-content">
-        <div 
+        <div
           className="footer-quote"
           data-aos="fade-up"
           data-aos-delay="100"
           data-aos-duration="600"
         >
           <h3>
-            {isEnglish 
+            {isEnglish
               ? "Where clean code meets exceptional user experience."
               : "حيث يلتقي الكود النظيف بتجربة المستخدم الاستثنائية."
             }
@@ -33,7 +33,7 @@ const Footer = ({ data, isEnglish }) => {
           </p>
         </div>
 
-        <div 
+        <div
           className="social-icons"
         >
           <a href="https://www.facebook.com/share/17vtfifLUB/" className="social-icon" target="_blank" rel="noopener noreferrer" title='Facebook'>
@@ -45,19 +45,19 @@ const Footer = ({ data, isEnglish }) => {
           <a href="https://wa.me/963997037470" className="social-icon" target="_blank" rel="noopener noreferrer" title='Whatsapp'>
             <i className="fab fa-whatsapp"></i>
           </a>
-          <a href="https://github.com/Rabee-Gh" className="social-icon" target="_blank" rel="noopener noreferrer" title="GitHub">
+          {/* <a href="https://github.com/Rabee-Gh" className="social-icon" target="_blank" rel="noopener noreferrer" title="GitHub">
             <i className="fab fa-github"></i>
-          </a>
+          </a> */}
           <a href={data.personalInfo.linkedin} className="social-icon" target="_blank" rel="noopener noreferrer" title="LinkedIn">
             <i className="fab fa-linkedin-in"></i>
           </a>
         </div>
 
-        <div 
+        <div
           className="copyright"
         >
           <p>
-            © {new Date().getFullYear()} {isEnglish ? data.personalInfo.nameEn : data.personalInfo.name}. 
+            © {new Date().getFullYear()} {isEnglish ? data.personalInfo.nameEn : data.personalInfo.name}.
             {isEnglish ? ' All rights reserved.' : ' جميع الحقوق محفوظة.'}
           </p>
         </div>
